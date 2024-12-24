33f5aa0f86bcbedabe0a5cd2f0ea2d49bd4e5b0c
swoojung7 <swoojung7@gmail.com>
2024-12-24 13:41:39 +0900

    install react-router-dom

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,20 @@
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home"
+import Counter from "./pages/Counter"
 
 function App() {
-  return <Home name='Post_SeSAC'/>;
+  return (
+    <div className="App">
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/counter">Counter</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home name="Post_SeSAC"/>} />
+        <Route path="/counter" element={<Counter />} />
+      </Routes>
+    </div>
+  );
+  // <Home name='Post_SeSAC'/>;
 }
 
 export default App;
