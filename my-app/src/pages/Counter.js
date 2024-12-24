96aa1204e14bb7e9d1336fe1a9f4b21d616bc670
swoojung7 <swoojung7@gmail.com>
2024-12-24 13:57:33 +0900

    Counter draft

--- my-app/src/pages/Counter.js
+++ my-app/src/pages/Counter.js
@@ -1,5 +1,27 @@
-function Counter() {
-  return <h1>Counter!</h1>;
-}
+import {useState} from 'react';
 
+const Counter = () => {
+  const [num, setNumber] = useState(0);
+
+  const increase = () => {
+    setNumber(num + 1);
+  }
+
+  const decrease = () => {
+    setNumber(num - 1);
+  }
+
+  const reset = () => {
+    setNumber(0)
+  }
+
+  return (
+    <div>
+      <button onClick={increase}>+1</button>
+      <button onClick={decrease}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <h1>{num}</h1>
+    </div>
+  )
+}
 export default Counter;
